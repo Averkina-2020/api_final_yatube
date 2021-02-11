68ca36c4373dad4dab269a1f435a92d6b43dff4f
Аверина Ольга <olga_averkina2000@mail.ru>
2021-02-11 22:30:43 +0300

    second commit and pep8

--- api/permissions.py
+++ api/permissions.py
@@ -7,7 +7,7 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return bool(
-            request.method in permissions.SAFE_METHODS 
+            request.method in permissions.SAFE_METHODS
             or request.user
             and request.user.is_authenticated
         )
